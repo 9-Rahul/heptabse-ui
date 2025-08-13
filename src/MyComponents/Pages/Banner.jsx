3d6056b271495d40678fa6d758c119eae2b63f82
Rahul Thapliyal <9.rahul02@gmail.com>
2025-08-14 04:24:48 +0530

    Banner updated
    -awards added
    -investor's logo added

--- src/MyComponents/Pages/Banner.jsx
+++ src/MyComponents/Pages/Banner.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import '../Styles/Banner.css'
+import landingImg from '../Assets/Images/Landing.webp'
+import investorOne from '../Assets/Images/YC.webp'
+import investorTwo from '../Assets/Images/hof.webp'
+import investorThree from '../Assets/Images/KleinerPerkins.webp'
+import investorFour from '../Assets/Images/Moving Capital.webp'
+
+
 
 const Banner = () => {
     return (
@@ -17,6 +24,24 @@ const Banner = () => {
 
 
                 <button className='btn-cntr-big'>Start your 7-day trial</button>
+            </div>
+
+            <div className="landing-img-container">
+                <img className='landing-img' src={landingImg} alt="landing-img" />
+            </div>
+
+            <div className="awards-container">
+                <span className="awrd-img-one"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=408608&theme=light&period=daily" alt="awrd-img-one" /></span>
+                <span className="awrd-img-two"> <img src="https://api.producthunt.com/widgets/embed-image/v1/golden-kitty-badge.svg?post_id=408608&theme=light" alt="" /></span>
+            </div>
+            <div className='blue-line'><span></span></div>
+            <h1 className='investors-texts'>Backed By</h1>
+            <div className="investors-logos">
+                <span><img src={investorOne} alt="investor's-logo" /></span>
+                <span><img src={investorTwo} alt="investor's-logo" /></span>
+                <span><img src={investorThree} alt="investor's-logo" /></span>
+                <span><img src={investorFour} alt="investor's-logo" /></span>
+
             </div>
         </section>
     )
